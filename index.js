9c1f746f5e214424226fa83ee0e300728adbb8f3
Anisol19 <imanisol19@gmail.com>
2023-10-04 22:50:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ const apiPaths = {
     fetchTrending:`${apiEndpoint}/trending/all/day?api_key=${apikey}&language=en-US`,
     searchOnYoutube: (query) => `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyC0SZJkHFX-fQ7NrsxdI4l4mGwYuY4l7P8`
 }
+
+// Boots up the app
+function init() {
+    fetchTrendingMovies();
+    fetchAndBuildAllSections();
+}
+
+function fetchTrendingMovies(){
+    fetchAndbuildMovieSection(apiPaths.fetchTrending, 'Trending Now')
+    .then(list => {
+        const randomIndex = parseInt(Math.random() * list.length);
+        buildBannerSection(list[randomIndex]);
+    }).catch(err=>{
+        console.error(err);
+    });
+}
